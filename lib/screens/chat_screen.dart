d724e163c13a02dcf8765fdcb9444e7cd6a3e137
NguyenDO <zmann22102002@gmail.com>
2023-07-22 23:10:26 +0700

    edit chat_screen, render conversation

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -10,33 +12,96 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final List<Map<String, String>> chatConversation = [];
+  final _formKey = GlobalKey<FormState>();
+  var _enteredQuestion = '';
+  void renderQuestion() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      setState(() {
+        chatConversation.add({'user': _enteredQuestion});
+        print('Length chatConversation: ');
+        print(chatConversation.length);
+        // print(_enteredQuestion);
+      });
+      _formKey.currentState!.reset();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat Coversation'),
+        title: const Text('Chat Coversation'),
       ),
-      body: Column(
+      body:
+          //  SingleChildScrollView(
+          //   child:
+          Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              const Expanded(
-                child: TextField(
-                  maxLength: 50,
-                  decoration: InputDecoration(
-                    label: Text('Enter a message'),
+          chatConversation.length != 0
+              ? Expanded(
+                  child: ListView.builder(
+                      itemCount: chatConversation.length,
+                      itemBuilder: (ctx, index) {
+                        final item = chatConversation[index];
+                        return Row(
+                          // mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Expanded(child: Icon(Icons.person)),
+                            Expanded(child: Text(item.values.first)),
+                          ],
+                        );
+                      }),
+                )
+              : Text('Hello'),
+          //     Row(
+          //           children: [
+          //             Expanded(
+          //               child: Icon(Icons.person),
+          //             ),
+          //             Expanded(
+          //               child: Text('Hello'),
+          //             ),
+          //             // Text(chatConversation[index]);
+          //           ],
+          //         ))
+          // : const Expanded(child: SizedBox(width: 30, height: 30)),
+          Form(
+            key: _formKey,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    maxLength: 50,
+                    decoration: const InputDecoration(
+                      label: Text('Enter a message'),
+                    ),
+                    validator: (value) {
+                      if (value == null ||
+                          value.isEmpty ||
+                          value.trim().length <= 1) {
+                        return 'A Short message!';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _enteredQuestion = value!;
+                    },
                   ),
                 ),
-              ),
-              SizedBox(
-                width: 5,
-              ),
-              ElevatedButton(onPressed: () {}, child: Icon(Icons.input))
-            ],
+                const SizedBox(
+                  width: 5,
+                ),
+                ElevatedButton(
+                    onPressed: renderQuestion, child: const Icon(Icons.input))
+              ],
+            ),
           )
         ],
+        // ),
       ),
     );
   }
